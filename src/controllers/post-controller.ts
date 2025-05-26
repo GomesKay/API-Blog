9d0681b262b8902202bc2cd885349d009f84183c
caiogomes <caio2011gs@gmail.com>
2025-05-26 11:25:40 -0300

    feat: Criação e Retorno de Post

--- src/controllers/post-controller.ts
+++ src/controllers/post-controller.ts
@@ -1,8 +1,34 @@
-export async function getPostsController() {}
+import { z } from "zod"
+import type { Request, Response } from "express"
+import { createPost, getAllPosts } from "../services/post-service"
+
+const createPostSchema = z.object({
+  title: z.string(),
+  content: z.string(),
+})
+
+export async function getPostsController(req: Request, res: Response) {
+  try {
+    const posts = await getAllPosts()
+
+    res.status(200).json(posts)
+  } catch (error) {
+    res.status(500).json({ error: "Erro ao obter os posts" })
+  }
+}
 
 export async function getPostController() {}
 
-export async function addPostController() {}
+export async function addPostController(req: Request, res: Response) {
+  try {
+    const { title, content } = createPostSchema.parse(req.body)
+    const newPost = await createPost({ title, content })
+
+    res.status(201).json(newPost)
+  } catch (error) {
+    res.status(500).json({ error: "Erro ao criar post" })
+  }
+}
 
 export async function updatePostController() {}
 
--- src/services/post-service.ts
+++ src/services/post-service.ts
@@ -1,8 +1,21 @@
-export async function getAllPosts() {}
+import { prisma } from "../lib/prisma"
+
+type CreatePostInput = {
+  title: string
+  content: string
+}
+
+export async function getAllPosts() {
+  return await prisma.post.findMany()
+}
 
 export async function getPostById() {}
 
-export async function createPost() {}
+export async function createPost({ title, content }: CreatePostInput) {
+  return await prisma.post.create({
+    data: { title, content },
+  })
+}
 
 export async function updatePost() {}
 
